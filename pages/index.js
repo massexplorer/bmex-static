ef8f13e4cdac45b10069869501a892f0531c55ba
Kyle Godbey <me@kyle.ee>
2021-10-21 13:55:53 -0400

    Play with adding plot page

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,13 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Explore masses in the Bayesian way!  
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://bmex.dev/plots" className={styles.card}>
+            <h2>Plots &rarr;</h2>
+            <p>Placeholder plot page! We can decide how we want to interact soon.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
