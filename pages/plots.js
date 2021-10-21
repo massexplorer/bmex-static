121769bed0e6cc2093dc52add53ec18518a80281
Kyle Godbey <me@kyle.ee>
2021-10-21 15:09:50 -0400

    Remove useeffect

--- pages/plots.js
+++ pages/plots.js
@@ -3,25 +3,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Plots() {
-  useEffect(() => {
-    Array.from(document.getElementsByTagName("iframe")).forEach((iframe) => {
-      iframe.contentWindow.addEventListener(
-        "load",
-        () => {
-          const doc = iframe.contentWindow.document;
-          iframe.height = doc.body.scrollHeight;
-        },
-        true
-      );
-      iframe.contentWindow.addEventListener(
-        "resize",
-        () => {
-          iframe.height = iframe.contentWindow.document.body.scrollHeight + 40;
-        },
-        true
-      );
-    });
-  }, []);
   return (
 	<iframe src="https://bmex.azurewebsites.net" height="100%" width="100%" title="BMEX App"></iframe>
   )
