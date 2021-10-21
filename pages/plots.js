c2ced3a96192529c40a02c6977b09cb3f220262c
Kyle Godbey <me@kyle.ee>
2021-10-21 14:54:23 -0400

    useEffect to deal with height

--- pages/plots.js
+++ pages/plots.js
@@ -3,6 +3,25 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Plots() {
+  useEffect(() => {
+    Array.from(document.getElementsByTagName("iframe")).forEach((iframe) => {
+      iframe.contentWindow.addEventListener(
+        "load",
+        () => {
+          const doc = iframe.contentWindow.document;
+          iframe.height = doc.body.scrollHeight;
+        },
+        true
+      );
+      iframe.contentWindow.addEventListener(
+        "resize",
+        () => {
+          iframe.height = iframe.contentWindow.document.body.scrollHeight + 40;
+        },
+        true
+      );
+    });
+  }, []);
   return (
 	<iframe src="https://bmex.azurewebsites.net" width="100%" title="BMEX App"></iframe>
   )
