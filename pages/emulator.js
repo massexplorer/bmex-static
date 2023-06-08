18e5819146b79bd1795498e70bd95cb4aebfd767
Kyle Godbey <me@kyle.ee>
2023-06-07 19:09:30 -0500

    Update emulator.js

--- pages/emulator.js
+++ pages/emulator.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Emulator() {
   return (
-	<iframe src="https://app.bmex.dev/emulator" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="Emulator"></iframe>
+	<iframe src="https://beta.bmex.dev/emulator" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="Emulator"></iframe>
   )
 }
