2e27d94f16002928d1b55891f79f3a3cfec24340
Kyle Godbey <me@kyle.ee>
2023-06-07 19:09:46 -0500

    Update gpe.js

--- pages/gpe.js
+++ pages/gpe.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function GPE() {
   return (
-	<iframe src="https://app.bmex.dev/gpe" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="BMEX App"></iframe>
+	<iframe src="https://beta.bmex.dev/gpe" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="BMEX App"></iframe>
   )
 }
