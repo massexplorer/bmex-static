0ad66626c2c91fb7d19784379ea038f55ec67c65
Kyle Godbey <me@kyle.ee>
2025-05-28 21:27:41 -0500

    Update masses.js

--- pages/masses.js
+++ pages/masses.js
@@ -4,6 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Masses() {
   return (
-	<iframe src="https://new.bmex.dev/masses" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="BMEX Masses" allow="clipboard-write"></iframe>
+	<iframe src="https://beta.bmex.dev/masses" style={{position: "absolute", height: "100%", border: "none"}} width="100%" title="BMEX Masses" allow="clipboard-write"></iframe>
   )
 }
